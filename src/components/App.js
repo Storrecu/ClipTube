e26de54db282b0d2aeb53332883ab968e78e5527
Silvia <silviatc1993@gmail.com>
2024-01-18 09:22:35 +0100

    setting videos as state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../services/youtube';
 
 import '../styles/App.scss';
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
   const onTermSubmit = async (term) => {
     console.log(term);
     const response = await youtube.get('/search', {
@@ -12,6 +14,8 @@ const App = () => {
         q: term,
       },
     });
+    console.log(response.data.items);
+    setVideos(response.data.items);
   };
 
   return (
@@ -19,6 +23,7 @@ const App = () => {
       <div className="ui container">
         <h1>Welcome to ClipTube</h1>
         <SearchBar onTermSubmit={onTermSubmit} />
+        {videos.length} videos found.
       </div>
     </>
   );
